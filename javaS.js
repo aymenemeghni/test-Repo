766bcb2bad061ba1ed9701a2e0d04df433b1a570
AYMENE <meghnimohamedaymene@gmail.com>
2025-02-05 15:35:22 +0100

    new changne

---
+++ javaS.js
@@ -0,0 +1,13 @@
+let aymene = {
+    p1 : "aymene chikour ",
+    p2 : 22 ,
+
+    m1 : function(x,y){
+        console.log(`hey ${x} you are ${y} years old`);
+    }
+
+}
+
+console.log(aymene.m1("ahmed",22));
+console.log(aymene.p1);
+console.log(aymene.p2);
